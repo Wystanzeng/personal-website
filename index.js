4414aa53270c96901fa9d993c41a616ad5fbbc14
Wystanzeng <53050221+Wystanzeng@users.noreply.github.com>
2021-01-13 19:17:14 +1000

    Update index.js

--- index.js
+++ index.js
@@ -12,3 +12,18 @@ $(".home__contact-button").click(function(){
     $("#page-active").removeAttr("id");
     $(".resume").attr("id", "page-active");
 })
+
+function initMap() {
+    // The location of Uluru
+    const uluru = { lat: -25.344, lng: 131.036 };
+    // The map, centered at Uluru
+    const map = new google.maps.Map(document.getElementById("map"), {
+      zoom: 4,
+      center: uluru,
+    });
+    // The marker, positioned at Uluru
+    const marker = new google.maps.Marker({
+      position: uluru,
+      map: map,
+    });
+  }
